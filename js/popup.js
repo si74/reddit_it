3d71a0eb3d6c7e2e8035db5a3153f95fda41fe66
Sneha Inguva <sneha.inguva@gmail.com>
2015-03-04 13:54:39 -0500

    Adjusting reddit url search

--- js/popup.js
+++ js/popup.js
@@ -15,7 +15,21 @@ $(document).ready(function(){
 			
 			$("#content").html(url);
 			
-			//search reddit for this url 
+			//search reddit for this url
+			 var url_encode = encodeURIComponent(url); 
+			 var url_search_string =  "https://www.reddit.com/search.json?q="+url_encode; 
+
+			 console.log(url_search_string);
+
+			 $.get(url_search_string, function( data ) {
+
+			 		console.log(data);
+			 		first_child = data['data']['children'][0];
+			 		
+			 		//get first set of comments from the returned list
+  					$( "#content" ).append(data);
+  					console.log('yes');
+			 });
 			
 		}
 						
